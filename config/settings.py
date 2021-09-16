0e080b7c6a1179ec34cfd4951062fb6c75e30158
jpwilson <jpwilson@hey.com>
2021-09-15 20:53:00 -0500

    add user reg: - Login - Logout - Signup

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+LOGIN_REDIRECT_URL = "home"  # or, can use the actual url with name 'home': "/"
+LOGOUT_REDIRECT_URL = "home"
--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django Admin
     path("admin/", admin.site.urls),
+    # User mgmt
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local apps
+    path("accounts/", include("accounts.urls")),
     path("", include("albums.urls")),
 ]
