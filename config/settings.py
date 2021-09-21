70c32d9e6201acebd0d9f02b974c21751b4459b5
jpwilson <jpwilson@hey.com>
2021-09-21 14:42:28 -0500

    complete renaming
    - all changes completed for move from albums to pages

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     #
     # Local
     "accounts",
-    "albums",
+    "pages",
 ]
 
 MIDDLEWARE = [
--- pages/apps.py
+++ pages/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class AlbumsConfig(AppConfig):
-    name = 'albums'
+class PagesConfig(AppConfig):
+    name = "pages"
